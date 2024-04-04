4f829ab02fd72da8e173fac460ee8c54729c8938
Johannes Bondzio <johannes.bondzio@materna.group>
2024-04-04 09:15:06 +0200

    CHG: correcting equations

--- 00-scipy-tutorial/runge-kutta-multidim.py
+++ 00-scipy-tutorial/runge-kutta-multidim.py
@@ -1,52 +1,66 @@
 from scipy.integrate import RK45
-import numpy as np
 import matplotlib.pyplot as plt
 import math
 
 # Vars
-alpha = 0.2
-productivity = 1
+productivity = 2.5
 consumption = 0.5
 price = 1
 dprice = 0
-interest_d = 1.08
-interest_a = 1.06
+interest_a = 0.08
+interest_d = 0.1
 
 def logistical(t, k, t0):
     return 1/(1+math.exp(-k*(t-t0)))
 
+def Rc(t):
+    if(t > 1 and t < 1.5):
+        return 16
+    else:
+        return 0
+
 def f(t, y):
     """this is the rhs of the ODE to integrate, i.e. dy/dt=f(y,t)"""
+    assets = y[0]
+    debt = y[1]
+    theta = assets * price * interest_a - debt * interest_d
+    En = price * (productivity - consumption) + theta
+    GammaR = 2 * logistical(debt, 1e1, 0) - 1
+    Rr = max(0., En * GammaR)
     return [
-        1/(1-alpha)*(productivity - consumption + y[0]*(interest_a - dprice/price) - y[1]/price*interest_d)*logistical(y[0], 1e2, 0), 
-        (2*y[1]*interest_d - y[0] * (price*interest_a - dprice) - price*(productivity - consumption))*logistical(y[1], 1e2, 0)
+        1 / price * (En + Rc(t) - Rr - assets * dprice), 
+        debt * interest_d + Rc(t) - Rr
         ]
 
-y0 = [1, 1]           # initial value y0=y(t0)
+assets0, debt0 = 0, 0
+y0 = [assets0, debt0]           # initial value y0=y(t0)
 t0 = 0             # integration limits for t: start at t0=0
-tf = 2     
+tf = 10     
 
 sol = RK45(fun=f, t0=t0, y0=y0, t_bound=tf, first_step=.02, max_step=.1, atol=1e-6, rtol=1e-6)
 
 t_values = []
 y0_values = []
 y1_values = []
-for i in range(100):
+Rc_values = []
+while True:
     # get solution step state
     sol.step()
     t_values.append(sol.t)
     y0_values.append(sol.y[0])
     y1_values.append(sol.y[1])
+    Rc_values.append(Rc(sol.t))
     # break loop after modeling is finished
     if sol.status == 'finished':
         break
 
-plt.close('all')
-fig, (ax1, ax2) = plt.subplots(2)
+fig, axs = plt.subplots(3)
 fig.suptitle('Vertically stacked subplots')
-ax1.plot(t_values, y0_values,'.')
-ax2.plot(t_values, y1_values,'.')
+axs[0].plot(t_values, y0_values,'.')
+axs[1].plot(t_values, y1_values,'.')
+axs[2].plot(t_values, Rc_values)
 plt.xlabel('t')
-ax1.set(ylabel='assets')
-ax2.set(ylabel='debt')
+axs[0].set(ylabel='assets')
+axs[1].set(ylabel='debt')
+axs[2].set(ylabel="Rc perturbation")
 plt.show()
